30c924061ff57d7d6638206bf4393e0d3ef54c9d
Syre <chestersyron.damian14@outlook.com>
2022-05-13 17:54:34 +0800

    Authentication : Devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # Temporary Root Routes
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
